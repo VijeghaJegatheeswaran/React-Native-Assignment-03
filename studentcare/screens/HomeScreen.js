f754c911b37a261590711caa9888386f241cd3db
VijeghaJegatheeswaran <139634071+VijeghaJegatheeswaran@users.noreply.github.com>
2025-01-06 06:58:25 +0000

    Home screen file created

---
+++ studentcare/screens/HomeScreen.js
@@ -0,0 +1,64 @@
+
+import { View } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import React from "react";
+import { Ionicons, MaterialCommunityIcons } from "@expo/vector-icons";
+import ProfileScreen from "./ProfileScreen";
+import CourseScreen from "./CourseScreen";
+import SubjectsScreen from "./SubjectsScreen";
+
+const Tab = createBottomTabNavigator();
+
+const HomeScreen = ({ route }) => {
+  const { student } = route.params;
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, size }) => {
+          let iconName;
+
+          if (route.name === "Profile") {
+            iconName = "person";
+          } else if (route.name === "Course") {
+            iconName = "school";
+          } else if (route.name === "Subjects") {
+            iconName = "book-open";
+            return (
+              <MaterialCommunityIcons
+                name={iconName}
+                size={size}
+                color={color}
+              />
+            );
+          }
+
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        headerShown: false,
+        tabBarActiveTintColor: "#70116d",
+        tabBarInactiveTintColor: "#888",
+        tabBarStyle: {
+          backgroundColor: "#fff",
+        },
+      })}
+    >
+      <Tab.Screen
+        name="Profile"
+        initialParams={{ student: student }}
+        component={ProfileScreen}
+      />
+      <Tab.Screen
+        name="Course"
+        component={CourseScreen}
+        initialParams={{ student: student }}
+      />
+      <Tab.Screen
+        name="Subjects"
+        component={SubjectsScreen}
+        initialParams={{ student: student }}
+      />
+    </Tab.Navigator>
+  );
+};
+
+export default HomeScreen;
